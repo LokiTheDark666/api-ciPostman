20c0d41a600555c3ce317ee3077e3b767a868c07
LokiTheDark666 <khcr.loki@mail.ru>
2022-07-27 13:42:46 +0300

    v1

---
+++ src/test/java/ru/netology/ApiTest2.java
@@ -0,0 +1,23 @@
+package ru.netology;
+
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
+class ApiTest2 {
+    @Test
+    void shouldReturnDemoAccounts() {
+
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .header("Content-Type", "application/json; charset=utf-8")
+                .contentType(ContentType.JSON)
+        ;
+    }
+}
